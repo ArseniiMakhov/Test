7d5d6582838ee1d7c32a8e8a8af9f6324f3591c5
ArseniiMakhov <arseniimakhov@gmail.com>
2025-01-09 12:35:14 +0300

    fixed*

--- src/Components/AppHeader/AppHeader.jsx
+++ src/Components/AppHeader/AppHeader.jsx
@@ -2,7 +2,7 @@ import classes from './AppHeader.module.scss'
 export const AppHeader = () => {
   return (
     <header className={classes['app-header']}>
-      <img src="/public/plane.png" alt="logo" />
+      <img src="/plane.png" alt="logo" />
     </header>
   )
 }
